527e64104d5b48916b972ead2277b9b78a6c8f4e
invalidtoken <codeit.sahil98@gmail.com>
2020-05-19 21:40:39 +0530

    using reducer, memo and useCallback

--- src/Grudges.js
+++ src/Grudges.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Grudge from './Grudge';
 
 const Grudges = ({ grudges = [], onForgive }) => {
+  console.log(grudges);
   return (
     <section className="Grudges">
       <h2>Grudges ({grudges.length})</h2>
-      {grudges.map(grudge => (
+      {grudges.map((grudge) => (
         <Grudge key={grudge.id} grudge={grudge} onForgive={onForgive} />
       ))}
     </section>
